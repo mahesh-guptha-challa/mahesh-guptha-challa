22ff672be6f0edf096165b848958b0be533614f9
mahesh-guptha-challa <challanzb@gmail.com>
2024-07-19 11:15:55 +0530

    latest changes

--- Django Books Project/books_project/books/admin.py
+++ Django Books Project/books_project/books/admin.py
@@ -1,20 +1,26 @@
 from django.contrib import admin
-from .models import Book, Author, Address
+from .models import Book, Author, Address, Country
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug", )
     prepopulated_fields = {"slug": ("title", )}
     list_filter = ("rating", "title")
-    list_display = ("id", "title", "rating", "author")
+    list_display = ("id", "title", "rating", "author", "country_published")
 
 class AuthorAdmin(admin.ModelAdmin):
 
     list_display = ("first_name", "last_name", )
     list_filter = ("first_name", )
 
+class AddressAdmin(admin.ModelAdmin):
+
+    list_filter = ('city', 'pincode')
+
+
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author, AuthorAdmin)
-admin.site.register(Address)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Country)
 
 # Need to create a second model and check whether it is working or not.
--- Django Books Project/books_project/books/models.py
+++ Django Books Project/books_project/books/models.py
@@ -2,10 +2,23 @@ from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 
+class Country(models.Model):
+    country_name = models.CharField(max_length=100)
+    country_code = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.country_name} {self.country_code}"
+
 class Address(models.Model):
     house_number = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
     pincode = models.CharField(max_length=6)
+
+    def __str__(self) -> str:
+        return f"{self.house_number} {self.city} {self.pincode}"
+
+    class Meta:
+        verbose_name_plural = "Address Entires"
 
     
 # Create your models here.
@@ -13,6 +26,7 @@ class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     address = models.OneToOneField(Address, on_delete=models.CASCADE, null = True)
+
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
@@ -25,6 +39,7 @@ class Book(models.Model):
     slug = models.SlugField(default="", null=False, db_index=True, unique=True)
     # models.CASCADE, models.SET_NULL, models.PROTECT, models.default
     author = models.ForeignKey(Author, on_delete=models.PROTECT, null=True, related_name="books")
+    country_published = models.ManyToManyField(Country)
 
     def path_of_the_url(self):  
         return reverse("model_detail", kwargs={"slug": self.slug})
